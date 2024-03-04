c520710d8930b0d4865cf834daf46b3b8c6d07ec
owen silberman <oreowen116@gmail.com>
2024-03-04 01:07:11 -0500

    Update email recipient and contact information

--- components/about.tsx
+++ components/about.tsx
@@ -19,30 +19,17 @@ export default function About() {
     >
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
-        After graduating with a degree in{" "}
-        <span className="font-medium">Accounting</span>, I decided to pursue my
-        passion for programming. I enrolled in a coding bootcamp and learned{" "}
-        <span className="font-medium">full-stack web development</span>.{" "}
-        <span className="italic">My favorite part of programming</span> is the
-        problem-solving aspect. I <span className="underline">love</span> the
-        feeling of finally figuring out a solution to a problem. My core stack
-        is{" "}
-        <span className="font-medium">
-          React, Next.js, Node.js, and MongoDB
-        </span>
-        . I am also familiar with TypeScript and Prisma. I am always looking to
-        learn new technologies. I am currently looking for a{" "}
-        <span className="font-medium">full-time position</span> as a software
-        developer.
+        I'm a <span className="font-medium">software developer</span> based in Charleston, Sc. I'm passionate about creating{" "}
+        <span className="font-medium">beautiful, functional, and accessible</span>{" "}
+        web applications. Aswell as fun Websites and games
       </p>
 
       <p>
         <span className="italic">When I'm not coding</span>, I enjoy playing
-        video games, watching movies, and playing with my dog. I also enjoy{" "}
+        football and {" "}
         <span className="font-medium">learning new things</span>. I am currently
-        learning about{" "}
-        <span className="font-medium">history and philosophy</span>. I'm also
-        learning how to play the guitar.
+        learning {" "}
+        <span className="font-medium">build games</span>
       </p>
     </motion.section>
   );
--- lib/data.ts
+++ lib/data.ts
@@ -5,6 +5,9 @@ import { LuGraduationCap } from "react-icons/lu";
 import corpcommentImg from "@/public/corpcomment.png";
 import rmtdevImg from "@/public/rmtdev.png";
 import wordanalyticsImg from "@/public/wordanalytics.png";
+import ai from "@/public/ai.png";
+import owenbucks from "@/public/owenbucks.png";
+import burt from "@/public/burt.png";
 
 export const links = [
   {
@@ -35,52 +38,52 @@ export const links = [
 
 export const experiencesData = [
   {
-    title: "Graduated bootcamp",
-    location: "Miami, FL",
+    title: "Started Coding",
+    location: "Charelston, SC",
     description:
-      "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
+      "Started Making simple websites and games. I was 10 years old when I started coding.",
     icon: React.createElement(LuGraduationCap),
-    date: "2019",
+    date: "2020",
   },
   {
-    title: "Front-End Developer",
-    location: "Orlando, FL",
+    title: "Front-End Developer For Myself",
+    location: "Charelston, SC",
     description:
-      "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
+      "Built owen bucks and crapchat. I used html, css, and js to build these projects.",
     icon: React.createElement(CgWorkAlt),
-    date: "2019 - 2021",
+    date: "2021-2022",
   },
   {
     title: "Full-Stack Developer",
-    location: "Houston, TX",
+    location: "Charelston, SC",
     description:
-      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
+      "I'm now a full-stack developer Still making stupid projects and games.",
     icon: React.createElement(FaReact),
-    date: "2021 - present",
+    date: "2022 - present",
   },
 ] as const;
 
 export const projectsData = [
   {
-    title: "CorpComment",
+    title: "Owen AI",
     description:
-      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
-    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
-    imageUrl: corpcommentImg,
+      "Simple Side Project I worked on for a couple of weeks. It's a web app that uses OpenAI's GPT-3.5 turbo to generate human-like text.",
+    tags: ["React", "Next.js", "Firebase", "Tailwind", "Vercel AI SDK"],
+    imageUrl: ai,
   },
   {
-    title: "rmtDev",
+    title: "Owen Bucks",
     description:
-      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
-    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
-    imageUrl: rmtdevImg,
+      "Simple crypto currency in html that let you create transactions and see the balance of the wallet. It uses Firebase to store the data.",
+    tags: ["Html", "Css", "Js", "Firebase"],
+    imageUrl: owenbucks,
   },
   {
-    title: "Word Analytics",
+    title: "Burt Coin",
     description:
-      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
-    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-    imageUrl: wordanalyticsImg,
+      "Simalar to owen Bucks a crypto currency in html that let you create transactions and see the balance of the wallet. It uses Firebase to store the data.",
+    tags: ["Html", "Css", "Js", "Firebase"],
+    imageUrl: burt,
   },
 ] as const;
 
@@ -94,14 +97,9 @@ export const skillsData = [
   "Node.js",
   "Git",
   "Tailwind",
-  "Prisma",
-  "MongoDB",
-  "Redux",
-  "GraphQL",
-  "Apollo",
+  "Firebase",
+  "Vercel Ai SDK",
   "Express",
-  "PostgreSQL",
   "Python",
   "Django",
-  "Framer Motion",
 ] as const;
